7f61a6afc09a9c83d4547ceed72a7dbe18fe22c5
Fabian Narvaez <narvaezfb4@hotmail.com>
2023-11-11 11:15:49 -0500

    improved version - validation added

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ logging.basicConfig(level=logging.INFO)
 
 EXIT_COMMAND = "0"
 CONFIRMATION_PROMPT = "\nAre you sure you want to delete these files? \nyes/no: "
+WELCOME_MESSAGE = "\t\tDuplicate File Remover\n"
+NOT_FILES_FOUND_MESSAGE = "\nNo duplicates files were found at this moment!\nEnter 'yes' to schedule the task "
+NOT_FILES_REMOVED = "\nNo files were removed in this batch!\n"
+GOOD_BYE_MESSAGE = "\nThanks for visiting, no files will be removed"
+JOB_TERMINATED_MESSAGE = "\nJob has been terminated! thanks.\n"
 
 
 def main():
     clear_console()
-    logging.info("\t\tDuplicate File Remover\n")
+    logging.info(WELCOME_MESSAGE)
 
     try:
         folderPath = ReceiveFolderPath()
@@ -35,8 +40,7 @@ def main():
                 if hasDuplicates(duplicateFiles):
                     DisplayFilesToDelete(duplicateFiles)
                 else:
-                    logging.error(
-                        "\nNo duplicates files were found at this moment!\nEnter 'yes' to schedule the task ")
+                    logging.error(NOT_FILES_FOUND_MESSAGE)
 
                 userInput = AskForConfirmation()
                 confirmationAccepted = ConfirmDelete(userInput)
@@ -47,30 +51,28 @@ def main():
 
                     while True:
                         clear_console()
-                        duplicateFiles = GetDuplicateFiles(folderPath)
-                        if hasDuplicates(duplicateFiles):
-                            logging.info("\n")
-                            RemoveDuplicates(duplicateFiles, folderPath)
-                            print('\n')
-                        else:
-                            logging.info(
-                                "\nNo files were removed in this batch!\n")
+                        try:
+                            duplicateFiles = GetDuplicateFiles(folderPath)
+                            if hasDuplicates(duplicateFiles):
+                                logging.info("\n")
+                                RemoveDuplicates(duplicateFiles, folderPath)
+                                print('\n')
+                            else:
+                                logging.info(NOT_FILES_REMOVED)
 
-                        DisplayExitTask()
-                        time.sleep(totalSeconds)
+                            DisplayExitTask()
+                            time.sleep(totalSeconds)
+                        except KeyboardInterrupt:
+                            logging.info(JOB_TERMINATED_MESSAGE)
+                            break
                 else:
                     clear_console()
-                    logging.info(
-                        "Thanks for visiting, no files will be removed")
-
-            else:
-                logging.error("\nFolder Does not exists, try again")
-
+                    logging.info(GOOD_BYE_MESSAGE)
         else:
-            logging.error("\nNo files have been deleted.\n")
+            logging.error(GOOD_BYE_MESSAGE)
 
     except KeyboardInterrupt:
-        logging.info("\nJob has been terminated! thanks.\n")
+        logging.info(JOB_TERMINATED_MESSAGE)
 
 
 if __name__ == "__main__":
